72344e14dd377d40d6d54cc406b22c864bd20115
FloweyAndinov <flowey.andinov@gmail.com>
2023-12-04 05:24:17 +0200

    Example Contact Page

--- src/components/pages/contacts/ContactsPage.tsx
+++ src/components/pages/contacts/ContactsPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Contacts from 'src/components/contact/Contacts'
 import { NavbarComp } from 'src/components/navbar/navbarcomp'
 
 const ContactsPage = () => {
   return (
     <>
     <NavbarComp/>
+    <Contacts/>
     </>
   )
 }
