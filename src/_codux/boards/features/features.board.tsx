3098fce30fe2c12e9934198462928972f088c720
FloweyAndinov <flowey.andinov@gmail.com>
2023-12-04 23:04:52 +0200

    adding dist

--- src/_codux/boards/features/features.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Features } from '../../../components/features/features';
-
-export default createBoard({
-    name: 'Features',
-    Board: () => <Features />,
-    isSnippet: true,
-});
