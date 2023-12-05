918df08b3e2437ed4265b066ebd967a8661b51b9
FloweyAndinov <flowey.andinov@gmail.com>
2023-12-05 04:10:12 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
     plugins: [react(), svgr() , tsconfigPaths()],
 
-    base: '/RoamPulse/',
+    base: "/RoamPulse/",
     server: {
       host: true
     },
