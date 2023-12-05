e108977c6426247b79922d0d2218c430362e1d48
FloweyAndinov <flowey.andinov@gmail.com>
2023-12-05 04:11:10 +0200

    HashRoute implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.js"
 
-import { createBrowserRouter , RouterProvider , Router } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/home/Home';
 import FeaturesPage from "./components/pages/features/FeaturesPage";
 import TrendingPage from "./components/pages/trending/TrendingPage";
@@ -11,50 +11,19 @@ import PricingPage from "./components/pages/pricing/PricingPage";
 
 
 function App() {
-   
-
-    const router = createBrowserRouter([
-        {
-            path : "/",
-            element :  <>
-                <Home/>
-             </>
-        },
-        {
-            path : "/features",
-            element : <>
-            <FeaturesPage/>
-            </>
-        },
-        {
-            path : "/trending",
-            element : <>
-            <TrendingPage/>
-            </>
-        },
-        {
-            path : "/pricing",
-            element : <>
-            <PricingPage/>
-            </>
-        },
-        {
-            path : "/contacts",
-            element : <>
-            <ContactsPage/>
-            </>
-        },
-        {
-            path : "/signin",
-            element : <>
-            <SignUpPage/>
-            </>
-        }
-    ])
-
     return (
-      <RouterProvider router={router}/>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/features" element={<FeaturesPage />} />
+          <Route path="/trending" element={<TrendingPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+          <Route path="/signin" element={<SignUpPage />} />
+          <Route path="/pricing" element={<PricingPage />} />
+          {/* Other routes go here */}
+        </Routes>
+      </HashRouter>
     );
-}
+  }
 
 export default App;
